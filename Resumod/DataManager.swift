e2d18ee6497a83532b4518b79cce9577b82b0909
Joseph Neuman <finder39@gmail.com>
2014-07-22 23:17:47 -0500

    Update to Gist with correctly formatted JSON

--- Resumod/DataManager.swift
+++ Resumod/DataManager.swift
@@ -30,7 +30,7 @@ class DataManager {
   
   // Using closure (swift equivilent of blocks)
   func getResume(#user_id:Int, onSuccess:(resume:ResumeModel)->(), onFailure:((error:NSError)->())?) {
-    let url = NSURL(string: "https://gist.githubusercontent.com/finder39/35336449bb3092a25821/raw/b2dd9d162d40ad8916dd4b1c34529deb8963d9fc/resume.json")
+    let url = NSURL(string: "https://gist.githubusercontent.com/finder39/35336449bb3092a25821/raw/3f82bd67ac6389ed41673ec399a1aeaf07b2b6d4/resume.json")
     var task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
       println("Task completed")
       if error {
@@ -38,9 +38,10 @@ class DataManager {
       }
       var err: NSError?
       var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as? Dictionary<String, AnyObject>
-      if err? {
+      if err {
         println("JSON Error \(err!.localizedDescription)")
       }
+      println(err)
       var results: Dictionary = jsonResult as Dictionary
       // here is where I'd call the callback or delegate method
       dispatch_async(dispatch_get_main_queue(), {
