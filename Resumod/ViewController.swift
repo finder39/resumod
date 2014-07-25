4fdf5a60b6fb3ae1ab56b9710f25ddfdb3ef929e
Joseph Neuman <finder39@gmail.com>
2014-07-25 12:09:22 -0500

    Added BioInfo to view

--- Resumod/ViewController.swift
+++ Resumod/ViewController.swift
@@ -14,19 +14,21 @@ class ViewController: UIViewController {
   @IBOutlet weak var labelLocation: UILabel!
   @IBOutlet weak var textSummary: UITextView!
   @IBOutlet weak var textSummaryHeightConstraint: NSLayoutConstraint!
+  @IBOutlet weak var textBioInfo: UITextView!
+  @IBOutlet weak var textBioInfoHeightConstraint: NSLayoutConstraint!
   @IBOutlet weak var imageGravatar: UIImageView!
                             
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     navigationController.navigationBar.barTintColor = CONSTANTS().color1
-    self.textSummaryHeightConstraint.constant = ceil(self.textSummary.sizeThatFits(CGSizeMake(self.textSummary.frame.size.width, CGFloat.max)).height)
     
     DataManager.sharedInstance.getResume(user_id: 1,
       onSuccess: {resume -> Void in
         dispatch_async(dispatch_get_main_queue(), {
           self.labelName.text = "\(resume.bio.firstName) \(resume.bio.lastName)"
           
+          // setup and display location
           var location = ""
           if let locationCity = resume.bio.location["city"] {
             location = locationCity
@@ -35,7 +37,24 @@ class ViewController: UIViewController {
             location += ", \(locationState)"
           }
           self.labelLocation.text = location
+          
+          // setup and display bio info
+          var bioInfo = ""
+          for (key, value) in resume.bio.email {
+            if !value.isEmpty {
+              bioInfo += "Email: \(value)"
+            }
+          }
+          for (key, value) in resume.bio.profiles {
+            if !value.isEmpty {
+              bioInfo += "\n\(key): \(value)"
+            }
+          }
+          self.textBioInfo.text = bioInfo
+          self.textBioInfoHeightConstraint.constant = ceil(self.textBioInfo.sizeThatFits(CGSizeMake(self.textBioInfo.frame.size.width, CGFloat.max)).height)
+          
           self.textSummary.text = resume.bio.summary
+          self.textSummaryHeightConstraint.constant = ceil(self.textSummary.sizeThatFits(CGSizeMake(self.textSummary.frame.size.width, CGFloat.max)).height)
           
           // get image
           let imageURL = "https://secure.gravatar.com/avatar/" + resume.bio.email["work"]!.lowercaseString.md5() + "?s=256&d=mm"
@@ -52,4 +71,3 @@ class ViewController: UIViewController {
   }
 
 }
-
